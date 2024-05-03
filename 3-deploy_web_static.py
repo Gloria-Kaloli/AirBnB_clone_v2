4ac1b143c2cbaff99bb204b734da1b38954d8577
Gloria-Kaloli <gloriakaloli00@gmail.com>
2024-05-03 15:55:22 -0700

    uodated DEPLOY STATIC

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ import re
 from datetime import datetime
 
 env.user = 'ubuntu'
-env.hosts = ["104.196.155.240", "34.74.146.120"]
+env.hosts = ["54.237.107.86", "18.204.7.130"]
 env.key_filename = "~/id_rsa"
 
 
